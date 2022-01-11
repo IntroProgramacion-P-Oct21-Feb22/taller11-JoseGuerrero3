1cdb9acbdd2b3299dbb86d61f177fc515254a06e
JoseGuerrero3 <jxguerrero3@utpl.edu.ec>
2022-01-10 21:48:37 -0500

    ´´

--- ejercicios/Problemas/src/problemas/Problema3.java
+++ ejercicios/Problemas/src/problemas/Problema3.java
@@ -5,6 +5,8 @@
  */
 package problemas;
 
+import java.util.Scanner;
+import java.util.Locale;
 /**
  *
  * @author reroes
@@ -16,6 +18,61 @@ public class Problema3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        Scanner entrada = new Scanner(System.in);
+
+        double[] nota = new double[4];
+        String cadena = "";
+        String nota_cualitativo = "";
+
+        for (int a = 0; a < nota.length; a++) {
+
+            System.out.println("Coloque las notas");
+            nota[a] = entrada.nextDouble();
+
+            nota_cualitativo = obtenerNotas(nota);
+            cadena = String.format("%s %.2f", cadena, nota[a]);
+        }
+
+        System.out.printf("Promedio de las notas: %s es %s\n",
+                 cadena,
+                 nota_cualitativo);
+
     }
-    
+
+    public static String obtenerNotas(double[] notas) {
+        
+        double promedio;
+        double suma = 0;
+        String ncualitativa = "";
+
+        for (int a = 0; a < notas.length; a++) {
+
+            suma = suma + notas[a];
+        }
+
+        promedio = suma / notas.length;
+
+        if (promedio >= 0 && promedio < 5) {
+            ncualitativa = "Regular";
+            
+        } else {
+            if (promedio >= 5 && promedio < 8) {
+                ncualitativa = "Bueno";
+                
+            } else {
+                if (promedio >= 8 && promedio < 9) {
+                    ncualitativa= "Muy Bueno";
+                    
+                } else {
+                    
+                    if (promedio >= 9 && promedio < 10) {
+                        ncualitativa = "Sobresaliente";
+                    }
+                }
+            }
+        }
+        return ncualitativa;
+
+    }
 }
--- ejercicios/Problemas/src/problemas/Problema5.java
+++ ejercicios/Problemas/src/problemas/Problema5.java
@@ -10,5 +10,29 @@ package problemas;
  * @author reroes
  */
 public class Problema5 {
+     public static void main(String[] args) {
+        
+        int sumaArreglo;
+        int[][] Informe = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        sumaArreglo = suma(Informe);
+        
+        System.out.printf("Suma del arreglo ingresado es:\n%d\n"
+                        , sumaArreglo);
+    }
     
+    public static int suma(int[][] a) {
+        
+        int suma = 0;
+        for (int filas = 0; filas < a.length; filas++) {
+            
+            for (int columnas = 0; columnas < a[0].length; columnas++) {
+                
+                suma = suma + a[filas][columnas];        
+                
+            }
+        }
+        return suma;
+    }
+
 }
+
